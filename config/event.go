6f2752ac2870c17770d877b90c638666719378d5
Oleh Volynets <saggot91@gmail.com>
2024-07-29 10:53:14 +0200

    feat: match events by key presence, being filled

--- config/event.go
+++ config/event.go
@@ -1,19 +1,82 @@
 package config
 
+import (
+	"errors"
+	"reflect"
+
+	"gopkg.in/yaml.v3"
+)
+
 type Event struct {
-	Name    string         `yaml:"name"`
-	Matcher map[string]any `yaml:"matcher"`
-	Scheme  []SchemeItem   `yaml:"scheme"`
+	Name    string       `yaml:"name"`
+	Matcher Matchers     `yaml:"matcher"`
+	Scheme  []SchemeItem `yaml:"scheme"`
 }
 
 func (evt *Event) Match(entry map[string]any) bool {
-	for key, val := range evt.Matcher {
-		param, ok := entry[key]
+	return evt.Matcher.Match(entry)
+}
+
+type Matcher struct {
+	key     string
+	value   any
+	Present bool `yaml:"present"`
+	Filled  bool `yaml:"filled"`
+}
+
+type Matchers struct {
+	ms []Matcher
+}
+
+func (container *Matchers) UnmarshalYAML(node *yaml.Node) error {
+	if node.Kind != yaml.MappingNode {
+		return errors.New("unsupported matcher format")
+	}
+
+	for i := 0; i < len(node.Content); i++ {
+		keyNode := node.Content[i]
+		valueNode := node.Content[i+1]
+		i++
+
+		matcher := Matcher{
+			key: keyNode.Value,
+		}
+
+		switch valueNode.Kind {
+		case yaml.ScalarNode:
+			matcher.value = valueNode.Value
+		case yaml.MappingNode:
+			if err := valueNode.Decode(&matcher); err != nil {
+				return err
+			}
+		}
+
+		container.ms = append(container.ms, matcher)
+	}
+
+	return nil
+}
+
+func (matchers *Matchers) Match(entry map[string]any) bool {
+	for _, m := range matchers.ms {
+		param, ok := entry[m.key]
 		if !ok {
 			return false
 		}
 
-		if param != val {
+		if m.Present || m.Filled {
+			if m.Filled {
+				if reflect.ValueOf(param).IsZero() {
+					return false
+				}
+
+				continue
+			}
+
+			continue
+		}
+
+		if param != m.value {
 			return false
 		}
 	}
